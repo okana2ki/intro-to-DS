a99802bc6389da143246ddbf611f86d0dfcbb230
Oka Natsuki <59405078+okana2ki@users.noreply.github.com>
2024-08-16 23:48:03 +0900

    Add files via upload

--- bingo_drum.py
+++ bingo_drum.py
@@ -2,6 +2,8 @@ import random
 import pygame
 import threading
 import time
+import requests
+import os
 
 # pygameを初期化
 pygame.init()
@@ -19,6 +21,18 @@ def bingo():
     Enterキーを押すたびに、1から18までの数字からランダムに当選番号を選び、
     それまでの当選番号と合わせて表示します。
     """
+    # URLとローカルファイル名を指定: https://github.com/okana2ki/intro-to-DS/blob/main/drum_roll.mp3
+    url = 'https://raw.githubusercontent.com/okana2ki/intro-to-DS/main/drum_roll.mp3'
+    local_filename = "local_drum_roll.mp3"
+
+    # ファイルをダウンロードしてローカルに保存
+    # response = requests.get(url)
+    # with open(local_filename, 'wb') as file:
+        # file.write(response.content)
+    response = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
 
     numbers = list(range(1, 19))  # 1から18までの数字のリスト
     drawn_numbers = []  # 当選番号を記録するリスト
@@ -30,7 +44,7 @@ def bingo():
         try:
             # Enterが押されたら、前のドラム再生を停止し、新しいドラム再生を開始
             stop_current_audio()  # 現在再生中の音声を停止; 前の音声再生が終わってなくても新しい音声再生を即座に開始できるよう。
-            play_mp3_with_timer("C:\Briefcase\__python\drum_roll.mp3", 3.5)
+            play_mp3_with_timer(local_filename, 3.5)
             # 3.5秒経ったら（シンバル音の後）当選番号を発表；音声再生は最後まで続く       
         except Exception as e:
             print(f"音声再生エラー: {e}")
